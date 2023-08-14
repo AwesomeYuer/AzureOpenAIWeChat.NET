4b85c47611e307be498c5f61a461dc7a51031cf3
AwesomeYuer <AwesomeYuer@Microshaoft.com>
2023-08-14 11:14:18 +0800

    sse

--- WebApi/Controllers/OpenAIController.cs
+++ WebApi/Controllers/OpenAIController.cs
@@ -3,6 +3,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using System.Text;
 
 [Route("api/[controller]")]
 [ApiController]
@@ -27,5 +28,31 @@ public class OpenAIController : ControllerBase
             Bot = ret,
             User = Topic.User
         };
+    }
+
+    [HttpPost]
+    [HttpGet]
+    [Route("sse")]
+    public async Task Post2()
+    {
+        HttpContext.Response.ContentType = "text/event-stream";
+        for (int i = 0; i < 10; i++)
+        {
+            string data = @$"id: {Guid.NewGuid()}
+retry: 1000
+event: message
+data: 你好{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}
+
+
+";
+            data = $"data: 你好{DateTime.Now:yyyy-MM-dd HH:mm:ss.fff}\n";    
+            var bitys = Encoding.UTF8.GetBytes(data);
+
+            await HttpContext.Response.Body.WriteAsync(bitys);
+            await HttpContext.Response.Body.FlushAsync();
+            //Task.Delay(1000 * 2);
+            Thread.Sleep(1000 * 5);
+        }
+        //return new EmptyResult();
     }
 }
